65f586cb47a678ad8b7606c6459c07155010e521
Athirson <athirson.lamonato@gmail.com>
2023-06-13 15:16:27 -0300

    Inserção de Form nos cards de produto

--- public/js/app/produtos.js
+++ public/js/app/produtos.js
@@ -1,9 +1,7 @@
 $(document).ready(function () {
-
   var divProducts = $("#products");
 
   const buscar_cliente = () => {
-
     const query = window.location.search;
     const params = new URLSearchParams(query);
 
@@ -18,41 +16,55 @@ $(document).ready(function () {
     parametros.push(csrf_value);
 
     return parametros;
-  }
+  };
 
   const buscar_produtos = (params) => {
     const csrfParams = `&csrf_name=${params[1]}&csrf_value=${params[2]}`;
     const url = `produtos/consultar?cliente=${params[0]}${csrfParams}`;
 
-
-
     fetch(url, { method: 'GET' })
       .then(response => response.json())
       .then(json => {
-
         json.forEach((val, i) => {
+          let html = '';
 
-          let html = `
-            <a href="leitor/arquivo?pdf=1${csrfParams}">
+          if (val.liberado == 'S') {
+            html = `
+              <form method="POST" action="leitor/arquivo">
+                <input type="hidden" name="pdf" value="${val.produto_id}">
+                <input type="hidden" class="valid" name="csrf_name" value="${params[1]}">
+                <input type="hidden" class="valid" name="csrf_value" value="${params[2]}">
+
+                <div class="card card-product mb-2">
+                  <div class="card-body"> 
+                    <div class="title-product">
+                      <h4>${val.nome_produto}</h4>
+                    </div>
+                  </div>
+                </div>
+
+                <button type"submit">teste</button>
+              </form>`;
+          } else {
+            html = `
               <div class="card card-product mb-2">
                 <div class="card-body"> 
                   <div class="title-product">
                     <h4>${val.nome_produto}</h4>
                   </div>
                 </div>
-              </div>
-            </a>`;
+              </div>`;
+          }
 
-          divProducts.append(html)
-
+          divProducts.append(html);
         });
+
       })
       .catch(error => {
         console.log(error);
       });
-  }
+  };
 
   const params = buscar_cliente();
   buscar_produtos(params);
-
 });
