8b32161f0a29f65f68c2443a1bfe6840531d7d50
G-Vieira <gianpvieira@gmail.com>
2023-06-16 11:02:37 -0300

    Alteração de viewer do js

--- app/views/leitor.php
+++ app/views/leitor.php
@@ -3,22 +3,21 @@
 <head>
   <title><?= $pdf ?></title>
 
-  <link rel="stylesheet" type="text/css" href="<?= $_ENV['BASE_PATH'] ?>/css/all.min.css">
   <link rel="stylesheet" type="text/css" href="<?= $_ENV['BASE_PATH'] ?>/css/bootstrap.min.css">
   <link rel="stylesheet" type="text/css" href="<?= $_ENV['BASE_PATH'] ?>/css/leitor.css?v=<?= time() ?>">
 
-  <script src="<?= $_ENV['BASE_PATH'] ?>/js/lib/all.min.js"></script>
-  <script src="<?= $_ENV['BASE_PATH'] ?>/js/lib/pdf.min.js"></script>
-  <script src="<?= $_ENV['BASE_PATH'] ?>/js/lib/pdf.worker.min.js"></script>
-  <script src="<?= $_ENV['BASE_PATH'] ?>/js/lib/bootstrap.min.js"></script>
+  <script src=" <?= $_ENV['BASE_PATH'] ?>/js/lib/jquery.min.js"></script>
+
 </head>
-<body oncontextmenu='return false'>
+<body ><!--oncontextmenu='return false'-->
 
   <?php if (isset($guard)) : ?>
     <input type="hidden" class="valid" id="pdf" value="<?= $pdf ?>">
     <input type="hidden" class="valid" id="<?= $guard->getTokenNameKey() ?>" value="<?= $guard->getTokenName() ?>">
     <input type="hidden" class="valid" id="<?= $guard->getTokenValueKey() ?>" value="<?= $guard->getTokenValue() ?>">
   <?php endif; ?>
+
+  <iframe id="pdf_viewer" onload="removerDownload()" src="<?= $_ENV['BASE_PATH'] ?>/js/lib/ViewerJS/#../../../pdf/<?= $pdf?>" allowfullscreen webkitallowfullscreen></iframe>
 
   <div id="pdf-viewer">
     <button onclick="goBack()" class="btn btn-back">Voltar</button>
@@ -28,8 +27,12 @@
     function goBack() {
       window.history.back();
     }
+
+    function removerDownload() {
+      $("#pdf_viewer").contents().find("#download").hide();
+    }
+    
   </script>
-  <script src="<?= $_ENV['BASE_PATH'] ?>/js/app/leitor.js?v=<?= time() ?>"></script>
 </body>
 
 </html>
