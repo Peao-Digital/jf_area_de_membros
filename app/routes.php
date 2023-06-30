957292d4216236394aa6897375905636b279f502
G-Vieira <gianpvieira@gmail.com>
2023-06-30 17:35:44 -0300

    ajuste de routes.php

--- app/routes.php
+++ app/routes.php
@@ -24,7 +24,7 @@
       return (new ProdutoController)->consultar($request, $response);
     })->add(new CsrfMiddleware($guard));
 
-    $app->any('/webhook', WebhookController::class)->add(new TokenMiddleware());
+    $app->any('/webhook', WebhookController::class);
     //->add(new TokenMiddleware())
 
     $app->any('/leitor', function($request, $response, $args) use ($app, $guard) {
